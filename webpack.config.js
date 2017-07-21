b76beb26a08f0a8ce575ee03d7a720a4a08e23ef
Client-Z <denisoleshenko@gmail.com>
2017-07-21 23:55:59 +0300

    Add file-loader, postcss-loader, plugin autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -9,66 +9,65 @@ module.exports = {
 		path: __dirname + "/dist",
 		filename: "bundle.js"
 	},
-	//watch: true,
-	devtool: 'source-map',
 	module:{
 		rules: [
 			{     		
-				
 				test: /\.js$/, 
 				enforce: "pre", 
 				exclude: /node_modules/,
-				use: [
+				use: [ 
 					{
-						loader: "jshint-loader"
+	                    loader: 'babel-loader',
+	                    options: {
+	                        presets: ['es2015']
+                    	}
+                    },
+					{
+						loader: 'jshint-loader'
 					}
 				]
-      },
+      		},
 			
 			{
 				test: /\.css$/,
 				use: [ 'style-loader', 'css-loader' ]
 			},
+
 			{
 				test: /\.scss$/,
+				enforce: "pre",
 				use: [{
 					    loader: "style-loader" // creates style nodes from JS strings 
 					}, {
 					    loader: "css-loader" // translates CSS into CommonJS 
 					}, {
+						loader: 'postcss-loader'	
+					}, {
 					    loader: "sass-loader" // compiles Sass to CSS 
 				}]
 			},
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-				}
-			}, 
+
 			{
           		test: /\.(png|svg|jpg|gif)$/,
           		use: [ 'file-loader' ]
         	}
 		]
 	},
+
 	plugins: [new UglifyJsPlugin({
 		minimize: true,
-		/*extractComments: true,*/
+		extractComments: true,
 		compress: {
 			warnings: false
 		},
-/*		output: {
+		output: {
 			comments: false
-		}*/
-	})],
+		}
+	}), require('autoprefixer')],
+
 	devServer: {
 		inline:true,
 		port: 8081
 	}
-
 	
 }
